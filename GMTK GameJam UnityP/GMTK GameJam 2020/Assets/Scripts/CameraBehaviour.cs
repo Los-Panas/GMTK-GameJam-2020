d8729b97df86da1522d00eeb310d4998b279d460
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-07-10 23:22:19 +0200

    Camera behaviour

---
+++ GMTK GameJam UnityP/GMTK GameJam 2020/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject Player;
+    public float smoothSpeed;
+    public Vector3 offset;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = Player.transform.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothPosition;
+    }
+}
