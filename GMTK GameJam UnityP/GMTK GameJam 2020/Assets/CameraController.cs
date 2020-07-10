62fc0bad37d9fcbd9af944d239828d0443f84e5d
Antoni Garí <antonigarichia@gmail.com>
2020-07-10 22:10:40 +0100

    CharacterController

---
+++ GMTK GameJam UnityP/GMTK GameJam 2020/Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 targetOffset;
+    [SerializeField] float movementSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MoveCamera();   
+    }
+
+    private void MoveCamera()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + targetOffset, movementSpeed * Time.deltaTime);
+    }
+}
