8dfc4c1f402eaa92feac64877656a6e9f5d99ea9
Antoni Garí <antonigarichia@gmail.com>
2020-07-10 22:58:28 +0100

    Firing

--- GMTK GameJam UnityP/GMTK GameJam 2020/Assets/Scripts/PlayerController.cs
+++ GMTK GameJam UnityP/GMTK GameJam 2020/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
 
+    [SerializeField] GameObject bulletSpawnPoint;
+    [SerializeField] float waitTime;
+    [SerializeField] GameObject bullet;
+    [SerializeField] int timeToFire;
+    private int toFireTrack = 0;
+    [SerializeField] bool ableToFire;
+
+    private Transform bulletSpawned;
+
     void Update()
     {
         MovementInput();
         RotationInput();
+        Shoot();
     }
 
     void MovementInput()
@@ -31,4 +41,21 @@ public class PlayerController : MonoBehaviour
             transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
         }
     }
+
+    void Shoot()
+    {
+        toFireTrack++;
+
+        if (toFireTrack == timeToFire && ableToFire)
+        {
+            toFireTrack = 0;
+            bulletSpawned = Instantiate(bullet.transform, bulletSpawnPoint.transform.position, Quaternion.identity);
+            bulletSpawned.rotation = bulletSpawnPoint.transform.rotation;
+        } else if (!ableToFire)
+        {
+            toFireTrack = 0;
+        }
+    }
+
+
 }
