a97ec5e83e4e60299a30127a7c71701a4e3b8807
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2020-07-11 00:20:02 +0200

    Camera limits

---
+++ GMTK GameJam UnityP/GMTK GameJam 2020/Assets/Scripts/CameraLimits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLimits : MonoBehaviour
+{
+    public static bool stop_camera;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Wall")
+        {
+            stop_camera = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "Wall")
+        {
+            stop_camera = false;
+        }
+    }
+}
