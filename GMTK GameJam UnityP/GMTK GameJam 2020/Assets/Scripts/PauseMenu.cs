fa18742e6121c9c7cddbd0200ca4971a23d19281
KydeHyle <46517230+KydeHyle@users.noreply.github.com>
2020-07-11 23:24:26 +0200

    PauseButton on P

---
+++ GMTK GameJam UnityP/GMTK GameJam 2020/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                Continue();
+            }
+         else {
+            Pause();
+        }
+        }
+    }
+
+   /* void Awake()
+    {
+        //Continue();
+        //pauseMenuUI.SetActive(true);
+        Time.timeScale = 1f;
+        //GameIsPaused = false;
+    }*/
+    public void Continue ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        //Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
