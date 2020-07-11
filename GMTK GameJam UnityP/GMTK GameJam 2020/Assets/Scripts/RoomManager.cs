a11407328be5f9c968c97e33fbb9065f5254240d
Lluís Moreu <36155329+youis11@users.noreply.github.com>
2020-07-11 03:08:52 +0200

    Wall Rotate, Room Manager WIP

---
+++ GMTK GameJam UnityP/GMTK GameJam 2020/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    private Transform PlayerTransform;
+
+    public Transform TeleportGoal;
+
+    public int enemySpawns = 0;
+
+    private int enemy = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerTransform = GameObject.Find("Player").transform;
+        enemy = enemySpawns;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (enemy.isdeleted)
+        // {
+        //      enemy = enemy - 1;
+        // }
+
+        if (enemy <= 0)
+        {
+            PlayerTransform.position = TeleportGoal.position;
+        }
+    }
+}
